15645303f765d380b9bc4a0422a3a5e277fe56ea
Jim Zhan <jim.zhan@me.com>
2019-03-12 23:01:26 +0800

    remove mongodb relative libs

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,3 @@
 import { ObjectId } from 'bson'
-import { connect, disconnect, register } from './connection'
-import DataLoader from './DataLoader.plugin'
 
 export const Key = () => (new ObjectId()).toHexString()
-
-export const plugins = {
-  DataLoader
-}
-export {
-  connect,
-  disconnect,
-  register
-}
